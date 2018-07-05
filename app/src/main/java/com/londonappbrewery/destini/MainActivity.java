435a4487c41add96a8a23a9d56601f62121d6823
GAURAV TYAGI <gtyagi88cse@gmail.com>
2018-07-06 03:23:04 +0530

    Started!

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,18 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    TextView mStoryTextView;
+    Button mTopButton;
+    Button mBottomButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +22,27 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
+        mTopButton = (Button) findViewById(R.id.buttonTop);
+        mBottomButton = (Button) findViewById(R.id.buttonBottom);
+        mStoryTextView = (TextView) findViewById(R.id.storyTextView);
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Destini","Top Button pressed!");
+                Toast.makeText(getApplicationContext(),"Top pressed",Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        mBottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Destini","Bottom Button pressed!");
+                Toast.makeText(getApplicationContext(),"Bottom pressed",Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
